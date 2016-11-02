6b4e0bcc76f9f61378fd8f90084d4aca682f9fc5
Aaron <aarongkendall@gmail.com>
2016-11-02 22:03:29 +0000

    type classes added

--- src/js/lib/htmlHelpers.js
+++ src/js/lib/htmlHelpers.js
@@ -1,13 +1,16 @@
+import typeLookup from './typeLookup';
+
 const htmlHelpers = {
     capitalise: function(string) {
         return string.charAt(0).toUpperCase() + string.slice(1, string.length);
     },
 
     renderPokemonCard: function(pokemon) {
-        return `<div class="card-item">
-                <img src="./assets/charmander.png" width="64px" height="64px" class="card-item-image" />
+        const type = typeLookup[pokemon.name];
+        return `<div class="card-item ${type}">
+                <img src="./assets/pokemon/${pokemon.name}.png" width="64px" height="64px" class="card-item-image" />
                 <span class="card-item-title">${this.capitalise(pokemon.name)}</span>
-                <p class="card-item-subtitle">${pokemon.type}</p>
+                <p class="card-item-subtitle">${type}</p>
             </div>`;
     },
 
